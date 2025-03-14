f0120dfc75a96ef1a57e8d2b7c2cbf3bb179d951
nanni00 <giovanni.malaguti00@gmail.com>
2025-03-14 15:59:35 +0100

    duckdb blend-like index creation

--- scripts/create_duckdb_index.py
+++ scripts/create_duckdb_index.py
@@ -1,15 +1,15 @@
-from itertools import chain
-import itertools
 import os
-import threading
+import pickle
+from itertools import chain
+
+import tqdm
+import bidict
 import duckdb
 import pandas as pd
 import polars as pl
 from wrapt_timeout_decorator import timeout
-import bidict
-import pickle
-from concurrent.futures import CancelledError, ThreadPoolExecutor, as_completed
-from tqdm import tqdm
+
+from orqa.utils import sanitize_string
 
 
 tables_path     = f'{os.path.dirname(__file__)}/../data/datasets/CAN/tables/tables_from10000_to15000'
@@ -22,94 +22,105 @@ if os.path.exists(db_path): os.remove(db_path)
 os.makedirs(os.path.dirname(db_path), exist_ok=True)
 
 # take the table IDs in alphabetical order
-table_ids = list(sorted(os.listdir(tables_path), reverse=True))
+table_ids = list(sorted(os.listdir(tables_path), reverse=True))[:100]
 
 # init the duckdb database
 con = duckdb.connect(db_path)
 
 con.execute(f"""
-    CREATE OR REPLACE TABLE OrqaIndex (
-       TableID      INT , 
-       ColumnID     INT , 
-       RowID        INT , 
-       Value        INT    
+    CREATE OR REPLACE TABLE AllTables (
+       TableId      INT , 
+       ColumnId     INT , 
+       RowId        INT , 
+       CellValue    INT    
        );
     """
 )
 
 values = {}
 
-NUM_WORKERS = 3
-n = CHECKPOINT = 10
+n = CHECKPOINT = 100    
 records = []
 
 
-@timeout(20)
-def get_unique_values(table_id):
-    lf = pl.scan_parquet(f'{tables_path}/{table_id}')
-    return set(chain(*(lf.select(col).unique().collect().get_column(col).drop_nans().drop_nulls().to_list() for col in lf.collect_schema().names())))
+# setting this timeout we will prob have some keyerror next
+# @timeout(20)
+def get_table_values(table_id):
+    df = pl.read_parquet(f'{tables_path}/{table_id}')
+    df = df[[s.name for s in df if not (s.null_count() == df.height)]]
 
-print('Create values dictionary')
-with ThreadPoolExecutor(NUM_WORKERS) as executor:
-    jobs = [executor.submit(get_unique_values, table_id) for table_id in table_ids]
+    return set(
+        filter(
+            lambda v: v not in values, 
+            map(
+                lambda s: sanitize_string(str(s)), 
+                chain(*(
+                    df.select(col).drop_nans().drop_nulls().unique().get_column(col).to_list() 
+                    for col in df.columns
+                    )
+                )                
+            )
+        )
+    )
+i = 0
+if not os.path.exists(values_path):
+    print('Create values dictionary')
     
-    for job in tqdm(as_completed(jobs), total=len(jobs)):
+    for table_id in tqdm.tqdm(table_ids):
         try:
-            unique = job.result()
-            for v in unique:
-                if v not in values:
-                    values[v] = len(unique)
+            unique_vals = get_table_values(table_id)
+            for v in unique_vals:
+                values[v] = i
+                i += 1
         except TimeoutError:
-            continue    
+            continue
     
+    values = bidict.bidict(values)
+
+    with open(values_path, 'wb') as fw:
+        pickle.dump(values, fw, protocol=pickle.HIGHEST_PROTOCOL)
+else:
+    print('Load values dictionary')
+    with open(values_path, 'rb') as fr:
+        values = pickle.load(fr)
+
 print(f'{len(values)=}')
 
-values = bidict.bidict(values)
-    
-with open(values_path, 'wb') as fw:
-    pickle.dump(values, fw, protocol=pickle.HIGHEST_PROTOCOL)
 
-
-
-@timeout(20)
+@timeout(5)
 def collect_table_records(table_idx, table_id):
-    df = pl.scan_parquet(f'{tables_path}/{table_id}').filter(~pl.all_horizontal(pl.all().is_null()) & ~pl.all_horizontal(pl.all().is_nan()))
-    
-    columns = df.collect_schema().names()
+    df = pl.read_parquet(f'{tables_path}/{table_id}')
 
     return [
-        [table_idx, col_idx, row_idx, values[cell]]
-        # for col_idx, col in tqdm(enumerate(columns), total=len(columns), disable=True, leave=False, position=(threading.get_native_id() % NUM_WORKERS) + 1)
-        # for row_idx, cell in tqdm(enumerate(df.select(col).collect().rows()), disable=True, total=df.select(pl.len()).collect().item(), leave=False, position=2)
-        for col_idx, col in enumerate(columns)
-        for row_idx, cell in enumerate(df.select(col).collect().rows())
-        
+        [table_idx, col_idx, row_idx, values[sanitize_string(str(cell))]]
+        for col_idx, col in enumerate(df.columns)
+        if not df.get_column(col).null_count() == df.height
+        for row_idx, cell in enumerate(df.select(col).get_column(col).to_list())        
         if not pd.isna(cell)
     ]
 
-print('Create index')
 
-with ThreadPoolExecutor(NUM_WORKERS) as executor:
-    jobs = [executor.submit(collect_table_records, table_idx, table_id) for table_idx, table_id in enumerate(table_ids)]
+for table_idx, table_id in tqdm.tqdm(enumerate(table_ids), total=len(table_ids)):
+    try:
+        records += collect_table_records(table_idx, table_id)
+        n -= 1
+    except TimeoutError:
+        continue
+    except KeyError:
+        continue
+    
+    if n == 0:
+        rec_df = pl.DataFrame(records, schema=['TableId', 'ColumnId', 'RowId', 'CellValue'], orient='row')
+        con.execute("INSERT INTO AllTables SELECT * FROM rec_df")
+        con.commit()
+        n = CHECKPOINT
+        records = []
 
-    for job in tqdm(as_completed(jobs), total=len(jobs)):
-        try:
-            records += job.result()
-            n -= 1
-        except TimeoutError:
-            continue
-
-        if n == 0:
-            rec_df = pl.DataFrame(records, schema=['TableID', 'ColumnID', 'RowID', 'Value'], orient='row')
-            con.execute("INSERT INTO OrqaIndex SELECT * FROM rec_df")
-            con.commit()
-            n = CHECKPOINT
-            records = []
-
-con.execute("CREATE INDEX IF NOT EXISTS TableID_idx   ON OrqaIndex (TableID);")
-con.execute("CREATE INDEX IF NOT EXISTS ColumnID_idx  ON OrqaIndex (ColumnID);")
-con.execute("CREATE INDEX IF NOT EXISTS RowID_idx     ON OrqaIndex (RowID);")
-con.execute("CREATE INDEX IF NOT EXISTS Value_idx     ON OrqaIndex (Value);")
-
-# with open(failures_path, 'w') as fw:
-#     fw.writelines(list(map(lambda)))
+rec_df = pl.DataFrame(records, schema=['TableId', 'ColumnId', 'RowId', 'CellValue'], orient='row')
+con.execute("INSERT INTO AllTables SELECT * FROM rec_df")
+con.commit()
+        
+con.execute("CREATE INDEX IF NOT EXISTS TableId_idx   ON AllTables (TableId);")
+con.execute("CREATE INDEX IF NOT EXISTS ColumnId_idx  ON AllTables (ColumnId);")
+con.execute("CREATE INDEX IF NOT EXISTS RowId_idx     ON AllTables (RowId);")
+con.execute("CREATE INDEX IF NOT EXISTS CellValue_idx ON AllTables (CellValue);")
